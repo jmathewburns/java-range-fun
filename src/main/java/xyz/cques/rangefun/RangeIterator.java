420a4a763f80e5dbf63054b9c5b14c1ffe7734b1
Jacques Burns <jmathewburns@gmail.com>
2017-06-21 19:41:56 +0200

    Move RangeIterator to its own class.

---
+++ src/main/java/xyz/cques/rangefun/RangeIterator.java
@@ -0,0 +1,42 @@
+package xyz.cques.rangefun;
+
+import java.util.NoSuchElementException;
+import java.util.PrimitiveIterator;
+
+/**
+ * Created by Jacques on 2017/06/21.
+ */
+class RangeIterator implements PrimitiveIterator.OfInt {
+    private final int end;
+    private int current;
+
+    RangeIterator(int start, int end) {
+        this.current = start;
+        this.end = end;
+    }
+
+    /**
+     * Returns the next {@code int} element in the iteration.
+     *
+     * @return the next {@code int} element in the iteration
+     * @throws NoSuchElementException if the iteration has no more elements
+     */
+    @Override
+    public int nextInt() {
+        return current++;
+    }
+
+    /**
+     * Returns {@code true} if the iteration has more elements.
+     * (In other words, returns {@code true} if {@link #next} would
+     * return an element rather than throwing an exception.)
+     *
+     * @return {@code true} if the iteration has more elements
+     */
+    @Override
+    public boolean hasNext() {
+        return current <= end;
+    }
+
+
+}
