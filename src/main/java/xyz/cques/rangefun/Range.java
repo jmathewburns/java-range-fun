8ee5d7903ab918894635d8b2fed201324d6098d7
Jacques Burns <jmathewburns@gmail.com>
2017-08-08 15:17:52 +0200

    Extend API for new functionality
    Added convenience methods for building various ranges.

--- src/main/java/xyz/cques/rangefun/Range.java
+++ src/main/java/xyz/cques/rangefun/Range.java
@@ -19,4 +19,118 @@ public class Range {
     public static Iterable<Integer> of(int start, int end) {
         return of(start, end, DEFAULT_STEP);
     }
+
+    public static RangeBuilder of(int start) {
+        return new RangeBuilder()
+                .from(start)
+                .step(DEFAULT_STEP);
+    }
+    
+    public static RangeBuilder of(Object[] array) {
+        return new RangeBuilder()
+                .from(0)
+                .to(array.length)
+                .step(DEFAULT_STEP)
+                .exclusiveEnd();
+    }
+
+    public static RangeBuilder of(int[] array) {
+        return new RangeBuilder()
+                .from(0)
+                .to(array.length)
+                .step(DEFAULT_STEP)
+                .exclusiveEnd();
+    }
+
+    public static RangeBuilder of(long[] array) {
+        return new RangeBuilder()
+                .from(0)
+                .to(array.length)
+                .step(DEFAULT_STEP)
+                .exclusiveEnd();
+    }
+
+    public static RangeBuilder of(float[] array) {
+        return new RangeBuilder()
+                .from(0)
+                .to(array.length)
+                .step(DEFAULT_STEP)
+                .exclusiveEnd();
+    }
+
+    public static RangeBuilder of(double[] array) {
+        return new RangeBuilder()
+                .from(0)
+                .to(array.length)
+                .step(DEFAULT_STEP)
+                .exclusiveEnd();
+    }
+
+    public static RangeBuilder of(byte[] array) {
+        return new RangeBuilder()
+                .from(0)
+                .to(array.length)
+                .step(DEFAULT_STEP)
+                .exclusiveEnd();
+    }
+
+    public static RangeBuilder of(char[] array) {
+        return new RangeBuilder()
+                .from(0)
+                .to(array.length)
+                .step(DEFAULT_STEP)
+                .exclusiveEnd();
+    }
+
+    public static RangeBuilder of(short[] array) {
+        return new RangeBuilder()
+                .from(0)
+                .to(array.length)
+                .step(DEFAULT_STEP)
+                .exclusiveEnd();
+    }
+    
+    public static Iterable<Integer> range(int start, int end, int step) {
+        return of(start, end, step);
+    }
+
+    public static Iterable<Integer> range(int start, int end) {
+        return of(start, end);
+    }
+
+    public static Iterable<Integer> range(int start) {
+        return of(start);
+    }
+
+    public static RangeBuilder range(Object[] array) {
+        return of(array);
+    }
+
+    public static RangeBuilder range(int[] array) {
+        return of(array);
+    }
+
+    public static RangeBuilder range(long[] array) {
+        return of(array);
+    }
+
+    public static RangeBuilder range(float[] array) {
+        return of(array);
+    }
+
+    public static RangeBuilder range(double[] array) {
+        return of(array);
+    }
+
+    public static RangeBuilder range(byte[] array) {
+        return of(array);
+    }
+
+    public static RangeBuilder range(short[] array) {
+        return of(array);
+    }
+
+    public static RangeBuilder range(char[] array) {
+        return of(array);
+    }
 }
--- src/test/java/xyz/cques/rangefun/APITest.java
+++ src/test/java/xyz/cques/rangefun/APITest.java
@@ -59,4 +59,95 @@ public class APITest {
 
         assertTrue(rangeReturnsExpectedInts(range, expectedIntegers));
     }
+
+    @Test
+    public void shouldReturnCorrectRangeOfIntsWithFluidAPI() {
+        Iterable<Integer> range = Range.of(0)
+                                        .to(5)
+                                        .step(2)
+                                        .exclusiveEnd();
+        int[] expectedIntegers = {0, 2, 4};
+
+        assertTrue(rangeReturnsExpectedInts(range, expectedIntegers));
+    }
+
+    @Test
+    public void shouldReturnCorrectRangeOfObjectArray() {
+        Object[] array = new Object[6];
+        int[] expectedIntegers = { 0, 1, 2, 3, 4, 5, };
+
+        Iterable<Integer> range = Range.of(array);
+
+        assertTrue(rangeReturnsExpectedInts(range, expectedIntegers));
+    }
+
+    @Test
+    public void shouldReturnCorrectRangeOfIntArray() {
+        int[] array = new int[6];
+        int[] expectedIntegers = { 0, 1, 2, 3, 4, 5, };
+
+        Iterable<Integer> range = Range.of(array);
+
+        assertTrue(rangeReturnsExpectedInts(range, expectedIntegers));
+    }
+
+    @Test
+    public void shouldReturnCorrectRangeOfLongArray() {
+        long[] array = new long[6];
+        int[] expectedIntegers = { 0, 1, 2, 3, 4, 5, };
+
+        Iterable<Integer> range = Range.of(array);
+
+        assertTrue(rangeReturnsExpectedInts(range, expectedIntegers));
+    }
+
+    @Test
+    public void shouldReturnCorrectRangeOfFloatArray() {
+        float[] array = new float[6];
+        int[] expectedIntegers = { 0, 1, 2, 3, 4, 5, };
+
+        Iterable<Integer> range = Range.of(array);
+
+        assertTrue(rangeReturnsExpectedInts(range, expectedIntegers));
+    }
+
+    @Test
+    public void shouldReturnCorrectRangeOfDoubleArray() {
+        double[] array = new double[6];
+        int[] expectedIntegers = { 0, 1, 2, 3, 4, 5, };
+
+        Iterable<Integer> range = Range.of(array);
+
+        assertTrue(rangeReturnsExpectedInts(range, expectedIntegers));
+    }
+
+    @Test
+    public void shouldReturnCorrectRangeOfByteArray() {
+        byte[] array = new byte[6];
+        int[] expectedIntegers = { 0, 1, 2, 3, 4, 5, };
+
+        Iterable<Integer> range = Range.of(array);
+
+        assertTrue(rangeReturnsExpectedInts(range, expectedIntegers));
+    }
+
+    @Test
+    public void shouldReturnCorrectRangeOfCharArray() {
+        char[] array = new char[6];
+        int[] expectedIntegers = { 0, 1, 2, 3, 4, 5, };
+
+        Iterable<Integer> range = Range.of(array);
+
+        assertTrue(rangeReturnsExpectedInts(range, expectedIntegers));
+    }
+
+    @Test
+    public void shouldReturnCorrectRangeOfShortArray() {
+        short[] array = new short[6];
+        int[] expectedIntegers = { 0, 1, 2, 3, 4, 5, };
+
+        Iterable<Integer> range = Range.of(array);
+
+        assertTrue(rangeReturnsExpectedInts(range, expectedIntegers));
+    }
 }
