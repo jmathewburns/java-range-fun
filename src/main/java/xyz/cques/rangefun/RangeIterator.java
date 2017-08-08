3e5f61cc07d398f4ba98ddf07707457d5bc9690d
Jacques Burns <jmathewburns@gmail.com>
2017-08-08 11:37:55 +0200

    Extend functionality of RangeIterator
    Added ability to iterate in reverse and/or with a custom step.

--- src/main/java/xyz/cques/rangefun/RangeIterator.java
+++ src/main/java/xyz/cques/rangefun/RangeIterator.java
@@ -7,12 +7,41 @@ import java.util.PrimitiveIterator;
  * Created by Jacques on 2017/06/21.
  */
 class RangeIterator implements PrimitiveIterator.OfInt {
+    private static final int DEFAULT_STEP = 1;
+
+    private boolean backwardsIteration;
+    private final int step;
     private final int end;
     private int current;
 
     RangeIterator(int start, int end) {
+        this(start, end, DEFAULT_STEP);
+    }
+
+    RangeIterator(int start, int end, int step) {
         this.current = start;
         this.end = end;
+
+        if (isBackwardsIteration(start, end)) {
+            backwardsIteration = true;
+            this.step = negative(step);
+        } else {
+            this.step = step;
+        }
+    }
+
+    private boolean isBackwardsIteration(int start, int end) {
+        return start > end;
+    }
+
+    private int negative(int number) {
+        int newNumber = number;
+
+        if (newNumber > 0) {
+            newNumber = -newNumber;
+        }
+
+        return newNumber;
     }
 
     /**
@@ -24,7 +53,9 @@ class RangeIterator implements PrimitiveIterator.OfInt {
     @Override
     public int nextInt() {
         if (hasNext()) {
-            return current++;
+            int oldCurrent = current;
+            current = current + step;
+            return oldCurrent;
         } else {
             throw new NoSuchElementException(
                     "the end of the range has been reached"
@@ -41,8 +72,9 @@ class RangeIterator implements PrimitiveIterator.OfInt {
      */
     @Override
     public boolean hasNext() {
+        if (backwardsIteration) {
+            return current >= end;
+        }
         return current <= end;
     }
-
-
 }
--- src/test/java/xyz/cques/rangefun/RangeIteratorTest.java
+++ src/test/java/xyz/cques/rangefun/RangeIteratorTest.java
@@ -12,9 +12,33 @@ import static xyz.cques.rangefun.RangeTestHelper.rangeReturnsExpectedInts;
  */
 public class RangeIteratorTest {
     @Test
-    public void shouldReturnExpectedInts() {
+    public void shouldReturnBasicRangeOfExpectedInts() {
         int[] expectedIntegers = { 1, 2, 3, };
         Iterator<Integer> rangeIterator = new RangeIterator(1, 3);
+
+        assertTrue(rangeReturnsExpectedInts(rangeIterator, expectedIntegers));
+    }
+
+    @Test
+    public void shouldReturnSteppedRangeOfExpectedInts() {
+        int[] expectedIntegers = { 1, 3, 5, };
+        Iterator<Integer> rangeIterator = new RangeIterator(1, 6, 2);
+
+        assertTrue(rangeReturnsExpectedInts(rangeIterator, expectedIntegers));
+    }
+
+    @Test
+    public void shouldReturnReversedRangeOfExpectedInts() {
+        int[] expectedIntegers = { 3, 2, 1, };
+        Iterator<Integer> rangeIterator = new RangeIterator(3, 1);
+
+        assertTrue(rangeReturnsExpectedInts(rangeIterator, expectedIntegers));
+    }
+
+    @Test
+    public void shouldReturnReversedSteppedRangeOfExpectedInts() {
+        int[] expectedIntegers = { 6, 4, 2, };
+        Iterator<Integer> rangeIterator = new RangeIterator(6, 1, 2);
 
         assertTrue(rangeReturnsExpectedInts(rangeIterator, expectedIntegers));
     }
